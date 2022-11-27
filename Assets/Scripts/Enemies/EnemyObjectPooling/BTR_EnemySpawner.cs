ed71df4ea3fe6905cee5daa683c91954cb0d317a
OniNoHanzo <HattoriIHanzo@outlook.com>
2022-11-27 08:06:45 -0600

    changes

--- Assets/Scripts/Enemies/EnemyObjectPooling/BTR_EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemyObjectPooling/BTR_EnemySpawner.cs
@@ -10,11 +10,11 @@ public class BTR_EnemySpawner : MonoBehaviour
 {
     public BTR_ObjectPooler objectPooler;
     public GameObject[] spawnPoints;
-    public bool isPlaying = false;
+    public bool isPlaying = true;
     public float radiusFromSpawnpoint;
     public string[] enemies = new string[]{"Ruler","TRuler","Squad","Transporter"};
 
-    private int _timing;
+    private int _timing = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -22,19 +22,20 @@ public class BTR_EnemySpawner : MonoBehaviour
         objectPooler = BTR_ObjectPooler.Instance;
         StartCoroutine(SpawnFromPaul(1));
     }
-    
 
-    public IEnumerator SpawnFromPaul(float timer)
+
+    private IEnumerator SpawnFromPaul(float timer)
     {
-        Debug.Log(enemies);
-        if (_timing <= 10)
+
+        while (isPlaying)
         {
-            yield return new WaitForSeconds(timer);
-            _timing++;
-        }
-        else if (_timing <= 70)
-        {
-            while (isPlaying)
+            Debug.Log(_timing);
+            if (_timing <= 10)
+            {
+                yield return new WaitForSeconds(timer);
+                _timing++;
+            }
+            else if (_timing <= 70)
             {
                 int random = Random.Range(0, spawnPoints.Length);
                 Vector3 radiusSpawn = new Vector3(Random.Range(-radiusFromSpawnpoint,radiusFromSpawnpoint),0,Random.Range(-radiusFromSpawnpoint,radiusFromSpawnpoint));
@@ -68,8 +69,14 @@ public class BTR_EnemySpawner : MonoBehaviour
                         break;
                 }
             
+            
             }
+            else
+            {
+                yield return new WaitForSeconds(timer);
+                _timing = 0;
+            }
+
         }
-        
     }
 }
